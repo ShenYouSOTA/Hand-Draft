d342bc97c938392840f6b462a329e0fe84b55d9e
Shen You <gaoming04@hotmail.com>
2024-09-25 20:04:20 +0800

    Alpha1.1

--- Codes HW1/Problem1.py
+++ Codes HW1/Problem1.py
@@ -5,31 +5,66 @@ n: The length of the sequence.
 m: The number of operations. 
 P: The divisor in modulo operation.
 """
+# Initialize
 n, m, P = int(basic_info[0]), int(basic_info[1]), int(basic_info[2])
 series: list = [int(num) for num in raw_series]
+summation = sum(series)
+distinct_set = set()
+for i in series:
+    distinct_set.add(i)
+    if series.count(i) > 1:
+        distinct_set.add(-i)
+distinct_value = len(distinct_set)
+
+
+def detect_withdraw(num: int) -> None:
+    global distinct_set
+    if num == 0:
+        if series.count(num) == 0:
+            distinct_set.remove(num)
+    else:
+        a = series.count(num)
+        b = series.count(-num)
+        if a + b < 2:
+            if a == 0:
+                distinct_set.remove(num)
+            else:
+                distinct_set.remove(-num)
+
+
+def detect_entry(num: int) -> None:
+    global distinct_set
+    a = series.count(num)
+    b = series.count(-num)
+    if (a+b) < 2 and b == 0:
+        num = -num
+    distinct_set.add(num)
 
 
 def update(factor: str) -> None:
+    global distinct_value, summation
     factor = factor.split()
     k = int(factor[1])
     x = int(factor[2])
     y = int(factor[3])
     c = int(factor[4])
     temp = x ** 2 + k * y + 5 * x
-    series[k - 1] = (temp % P) * c
+    current = (temp % P) * c
+
+    previous = series.pop(k-1)
+    detect_withdraw(previous)
+    detect_entry(current)
+    series.insert(k - 1, current)
+    summation = summation - previous + current
+    distinct_value = len(distinct_set)
 
 
 def sum_sequence() -> None:
-    summation = sum(series)
     print(summation)
 
 
-def distinct_value() -> None:
-    distinct_set = set()
-    for num in series:
-        if num not in distinct_set and -num not in distinct_set:
-            distinct_set.add(num)
-    print(len(distinct_set))
+def count_distinct() -> None:
+    print(distinct_value)
 
 
 while 0 < m:
@@ -37,7 +72,7 @@ while 0 < m:
     if command == "2":
         sum_sequence()
     elif command == "3":
-        distinct_value()
+        count_distinct()
     else:
         update(command)
     m -= 1
--- Codes HW1/Problem2.py
+++ Codes HW1/Problem2.py
@@ -1,11 +1,27 @@
 constraint = input("")
 n = int(constraint[0])
 q = int(constraint[1])
-permutation = input("")
-permutation.split()
-left_piece = input("")
-left_piece.split()
-right_piece = input("")
-right_piece.split()
+access = False
+coverage = False
+
+permutation = input("").split()
+left_piece = input("").split()
+right_piece = input("").split()
+
+
+def fail_test(L, R):
+    try:
+        index_L = permutation.index(L)
+        index_R = permutation.index(R)
+        if index_L >= index_R:
+            return True
+        return False
+    except ValueError:
+        return True
+
+
 for i in range(1, q + 1):
     pivot = -i
+    left, right = left_piece[pivot], right_piece[pivot]
+    if fail_test(left, right):
+        break
